9971ca404c3ed45a654c0ac677f1c2564ad1b60f
callanbr <chrisallan1992@icloud.com>
2020-10-26 08:42:32 -0700

    added #2 and #3 Starter

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import Starter.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
---
+++ src/Starter/Starter.java
@@ -0,0 +1,33 @@
+package Starter;
+
+public class Starter {
+
+    // #1 - SumDoubler
+    // Given the two int values, return their sum. If the two values are the same,
+    // then return the sum doubled.
+    // Example: 1+2 = 3; 2+2= 4*2 = 8;
+
+    public static int SumDoubler(int a, int b) {
+        int sum = 0;
+        return sum;
+    }
+
+    // #2 - SleepingIn
+    // Weekday is true if it is a weekday.
+    // Vacation is true if we are on vacation.
+    // We sleep in if it is not a weekday or we're on vacation.
+    // Return true if we sleep in.
+
+    public static boolean SleepingIn(boolean weekday, boolean vacation) {
+        return false;
+    }
+
+    // #3 - AllOrNothing
+    // Return true if one and two are both true or both false.
+    // If they are not both true or false - return false.
+
+    public static boolean AllOrNothing(boolean one, boolean two) {
+        return false;
+    }
+
+}
